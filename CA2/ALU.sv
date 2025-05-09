e760c3079e7fef49f95d9832f5deefe0fc080839
manih1384 <manihosseini1384@gmail.com>
2025-05-09 10:43:15 +0330

    added CU and a few other modules

---
+++ CA2/ALU.sv
@@ -0,0 +1,20 @@
+module ALU (
+    input [31:0] SrcA, SrcB,
+    input [2:0] ALUControl,
+    output reg [31:0] ALUResult,
+    output zero
+);
+    always @(SrcA,SrcB,ALUControl) begin
+        case (ALUControl)
+            3'd0: ALUResult = SrcA + SrcB;      
+            3'd1: ALUResult = SrcA - SrcB;      
+            3'd2: ALUResult = SrcA & SrcB;      
+            3'd3: ALUResult = SrcA | SrcB;      
+            3'd4: ALUResult = (SrcA < SrcB) ? 32'd1 : 32'd0;  
+            3'd5: ALUResult = SrcA ^ SrcB;     
+            default: ALUResult = 32'd0;
+        endcase
+    end
+    
+    assign zero = (ALUResult == 32'd0);
+endmodule
