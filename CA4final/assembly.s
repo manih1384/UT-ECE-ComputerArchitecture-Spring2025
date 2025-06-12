f3f97d7ac4eb3f42535dfd5d9cc37026495514fb
Maleki Shayan <malekishayan86@gmail.com>
2025-06-12 17:49:24 +0330

    asd

--- CA4final/assembly.s
+++ CA4final/assembly.s
@@ -1,28 +1,19 @@
-    lw   x10, 0(x0)       # x10 = max value (first element)
-    ori  x11, x0, 0       # x11 = max index = 0
-    ori  x12, x0, 1       # x12 = current index = 1
-    ori  x13, x0, 20      # x13 = array size = 20
-
-loop:
-    beq  x12, x13, done   # if index == 20, exit
-    
-    # Calculate index * 4 using additions
-    add  x14, x12, x12    # x14 = index * 2
-    add  x14, x14, x14    # x14 = index * 4
-    
-    add  x15, x0, x14     # x15 = address of array[index]
-    lw   x16, 0(x15)      # x16 = array[index]
-    
-    slt  x17, x10, x16    # x17 = 1 if max < array[index]
-    beq  x17, x0, skip    # if not greater, skip update
-    
-    add  x10, x16, x0     # update max value
-    add  x11, x12, x0     # update max index
-
-skip:
-    addi x12, x12, 1      # index++
-    jal  x0, loop         # jump back to loop
-    
-done:
-    sw   x10, 80(x0)      # store max value at address 80
-    sw   x11, 84(x0)      # store max index at address 84
+lw x8, 1000(x0)         # x8 = data[0]
+add x9, x0, x0          # x9 = 0 (index pointer)
+add x15, x0, x0         # x15 = 0 (max index)
+Loop:
+addi x9, x9, 4          # x9 += 4 (move to next 32-bit element)
+slti x6, x9, 80         # t1 = (x9 < 80)
+beq x6, x0, EndLoop     # branch if done
+lw x18, 1000(x9)        # x18 = data[index]
+slt x6, x8, x18         # t1 = (max < current)
+beq x6, x0, Loop        # if not, skip update
+add x8, x18, x0         # max = x18
+add x15, x9, x0         # max_index = x9
+jal x0, Loop            # always jump to Loop
+EndLoop:
+sw x8, 2000(x0)         # store max to address 2000 (first answer)
+sw x15, 2004(x0)        # store max_index to address 2004 (second answer)
+jal x0, End             # infinite loop/halt
+End:
+jal x0, End
